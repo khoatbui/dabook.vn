c07ff7e55d6bcd6b530dee2fdd72eb597a0ab3c9
khoatbui <khoatbui.dev@gmail.com>
2019-09-20 00:10:55 +0700

    Fix loi owl xuong dong

--- desktop.dabook.vn/js/custom.js
+++ desktop.dabook.vn/js/custom.js
@@ -20,7 +20,27 @@ $('.owl__topdes').owlCarousel({
     '<i class="fas fa-chevron-right"></i>',
   ],
 });
-
+$('.owl__favoritedes').owlCarousel({
+  items: 5,
+  loop: true,
+  margin: 10,
+  merge: true,
+  nav: true,
+  dots: false,
+  lazyLoad: true,
+  responsive: {
+    678: {
+      mergeFit: true,
+    },
+    1000: {
+      mergeFit: false,
+    },
+  },
+  navText: [
+    '<i class="fas fa-chevron-left"></i>',
+    '<i class="fas fa-chevron-right"></i>',
+  ],
+});
 $('.owl__todaypro').owlCarousel({
   items: 3,
   loop: true,
@@ -129,9 +149,9 @@ function scrollFunction() {
   } else {
     scrollTopBtn.style.display = 'none';
   }
-  if (bookingSticky !=null && (
-    document.body.scrollTop > 700 ||
-    document.documentElement.scrollTop > 700)
+  if (
+    bookingSticky != null &&
+    (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700)
   ) {
     bookingSticky.style.display = 'block';
   } else {
@@ -145,17 +165,21 @@ function topFunction() {
 
 // ========CHANGE DATA MERGE ON HOVER========
 $(document).ready(function() {
-  var originWidth = $('.owl__topdes .owl-item').width();
+  const originWidth = $('.owl__topdes .owl-item').width();
+  const rootOwlCarousel = $('.owl__topdes .owl-stage').width();
   $('.owl__topdes .owl-item').hover(
     function() {
       $('.owl__topdes .owl-item:not(:hover)').animate(
         { width: originWidth },
         400
       );
+      $('.owl__topdes .owl-stage').width(rootOwlCarousel + originWidth);
       // $(this).width(originWidth * 2);
       if ($(this).width() == originWidth) {
         $(this).animate({ width: originWidth * 2 }, 400);
-        $(this).addClass('active');
+        $(this)
+          .addClass('active')
+          .removeClass('cloned');
       }
     },
     function() {
